a8ba2c50f0b54a281a39a33b3326905b2f359e0a
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2025-04-21 16:46:25 -0400

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,19 @@
+function confirmReset(element){
+    document.getElementById(element).style.color = "#009655"
+    document.getElementById(element).style.backgroundColor = "#dda809"
+    document.getElementById(element).innerHTML = "Successfully Reset!"
+}
+
 function resetHighscore(){
     localStorage.removeItem("bestScore")
-    document.getElementById("reset").style.color = "#009655"
-    document.getElementById("reset").style.backgroundColor = "#dda809"
-    document.getElementById("reset").innerHTML = "Successfully Reset!"
+    confirmReset("resetHigh")
+}
+
+function resetAll(){
+    localStorage.removeItem("bestScore")
+    localStorage.removeItem("coins")
+    localStorage.removeItem("playerShopInfo")
+    localStorage.removeItem("kiteColor")
+    localStorage.removeItem("kiteSpeed")
+    confirmReset("resetAll")
 }
